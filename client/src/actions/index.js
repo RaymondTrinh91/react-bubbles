9fc4ab9c77a66a72b164faa36b197ef0c0e584ab
Raymond Trinh <RaymondTrinh91@gmail.com>
2019-11-21 01:11:43 -0800

    Finished Campaigns/ starting Donors

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,8 +9,7 @@ export const postLogin = (user) => dispatch => {
     axiosWithAuth()
         .post('http://localhost:5000/api/login', user)
         .then(res => {
-            console.log(res)
-            localStorage.setItem('token', res);
+            localStorage.setItem('token', res.data.payload);
             dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload })
         })
         .catch(err => dispatch({ type: LOGIN_FAILURE, payload: err.response.data.error }))
